194c581065a3af65a386283d6736bc90dc67498e
ShashiMadari <shashimadari44@gmail.com>
2025-05-11 18:30:52 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -39,27 +39,4 @@ public class App {
         driver.quit();
     }
 }
-package com.example;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class App {
-    public static void main(String[] args) {
-        // Set path to ChromeDriver
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-
-        // Initialize WebDriver
-        WebDriver driver = new ChromeDriver();
-
-        // Open the SauceDemo site
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
-
-        // Perform login
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-    }
-}
